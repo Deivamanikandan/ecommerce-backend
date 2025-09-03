d9a0784612af698c9ad82c72ac39d6e09e58901f
Deivamanikandan <dheiva2016@gmail.com>
2025-09-03 23:28:28 +0530

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-// src/db.js
-require('dotenv').config();
-const knex = require('knex');
-const db = knex({
-  client: 'pg',
-  connection: process.env.DATABASE_URL,
-  pool: { min: 0, max: 10 }
-});
-module.exports = db;
